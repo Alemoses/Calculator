6481933a05b24090f7b84237c5e216a80ca89019
Alex <alexnowrang@gmail.com>
2023-11-19 13:29:10 -0600

    Add functions to render the display from click events

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ let secondNum = '';
 let operator = '';
 let result = 0;
 
+const clearBtn = document.querySelector('#clear');
+const deleteBtn = document.querySelector('#delete');
+const display = document.querySelector('#display');
+const operatorBtn = document.querySelectorAll('.operator');
+const numberBtn = document.querySelectorAll('.number');
+const equalsBtn = document.querySelector('#equals');
+const decimalBtn = document.querySelector('#decimal');
+
 function add(a, b) {
   return a + b;
 }
@@ -38,4 +46,33 @@ function operate(numA, operator, numB) {
   };
   return result;
 }
+
+//renders numbers to the display when corresponding numbered button is clicked and saves them to variables when an operator button is clicked
+function displayNums() {
+  numberBtn.forEach(number => {
+    number.addEventListener("click", e => {
+      display.value += e.target.value;
+      if (operator === '') {
+        firstNum += e.target.value;
+        console.log(firstNum)
+      } else {
+        secondNum += e.target.value;
+        console.log(secondNum)
+      }
+    });
+  });
+}
+
+//renders operators to the display when corresponding operator is clicked
+function displayOperators() {
+  operatorBtn.forEach(op => {
+    op.addEventListener("click", e => {
+      display.value += e.target.value;
+      operator = e.target.value;
+      secondNum = '';
+      console.log(operator)
+    });
+  });
+}
+
 
