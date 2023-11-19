0c7986629dbfddc8319e7a84896c52ae745178e3
Alex <alexnowrang@gmail.com>
2023-11-19 13:06:43 -0600

    Create function that calls the math functions

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+let firstNum = '';
+let secondNum = '';
+let operator = '';
+let result = 0;
+
 function add(a, b) {
   return a + b;
 }
@@ -14,4 +19,23 @@ function divide(a, b) {
   return a / b;
 }
 
+function operate(numA, operator, numB) {
+  if (operator === '+') {
+    result = add(numA, numB);
+    console.log(add(numA, numB));
+  } else if (operator === '-') {
+    result = subtract(numA, numB);
+    console.log(subtract(numA, numB));
+  } else if (operator === '*' || operator === 'x') {
+    result = multiply(numA, numB);
+    console.log(multiply(numA, numB));
+  } else {
+    result = divide(numA, numB);
+    console.log(divide(numA, numB));
+    if (numB === 0) {
+      return "Can't divide by 0";
+    }
+  };
+  return result;
+}
 
